02d240ea75b7c51115780a05826d2f9daa1f3661
OngTK <dhdxorud@gmail.com>
2025-06-12 11:54:37 +0900

    250612_1154

--- 00_실습/02_JS/05_실습5_20250612/실습05.js
+++ 00_실습/02_JS/05_실습5_20250612/실습05.js
@@ -26,11 +26,19 @@ if ( fruitList.indexOf(newFruit) >= 0 ) {
 성별을 나타내는 7번째 숫자가 '1' 또는 '3'이면 'blue'를, 
 '2' 또는 '4'이면 'red'를 출력하는 프로그램을 작성하시오.
 */
+/*
+let id_num = prompt("주민등록번호 13자리를 입력해주세요.")
 
-/*
-let id_num = prompt("주민번호 13자리를 입력해주세요.")
-console.log(id_num.slice(6,7))
-/*
+// 텍스트도 index로 중간 글자를 가져올 수 있다!!!!!!!!!!!!!!!!!!!!!!!!
+
+if (id_num[6] == 1 || id_num[6] == 3) {  
+    console.log('blue')
+} else if (id_num[6] == 2 || id_num[6] == 4) {
+    console.log('red')
+} else {
+    console.log('주민등록번호가 올바르지 않습니다.')
+}
+*/
 
 /* ==============================
 문제 3: 구매 금액에 따른 할인율 적용
@@ -171,25 +179,81 @@ if (num7_1 < num7_2 && num7_1 < num7_3) {
 */
 
 /*
-let player8_1 = Number(prompt("0(가위), 1(바위), 2(보)"));
-let player8_2 = Number(prompt("0(가위), 1(바위), 2(보)")); 
+let player8_1 = Number(prompt("플레이어 1 : 0(가위), 1(바위), 2(보)"));
+let player8_2 = Number(prompt("플레이어 2 : 0(가위), 1(바위), 2(보)")); 
+
+if (player8_1 == 0) {
+    if (player8_2 == 0) {
+        console.log("무승부");
+    } else if (player8_2 == 1) {
+        console.log("플레이어 2 승리");
+    } else (
+        console.log("플레이어 1 승리");
+    )
+} else if (player8_1 == 1) {
+    if (player8_2 == 0) {
+        console.log("플레이어 1 승리");
+    } else if (player8_2 == 1) {
+        console.log("무승부");
+    } else (
+        console.log("플레이어 2 승리");
+    )
+} else {
+    if (player8_2 == 0) {
+        console.log("플레이어 2 승리");
+    } else if (player8_2 == 1) {
+        console.log("플레이어 1 승리");
+    } else (
+        console.log("무승부");
+    )
+}
 */
-
-
-
 
 /* ==============================
 문제 9: 주차 차량 위치 검색
-주차장 차량 정보가 다음과 같이 두 개의 배열로 관리되고 있습니다. 두 배열에서 같은 인덱스는 동일한 차량의 정보를 의미합니다.
+주차장 차량 정보가 다음과 같이 두 개의 배열로 관리되고 있습니다. 
+두 배열에서 같은 인덱스는 동일한 차량의 정보를 의미합니다.
 차량 번호 목록: let carArray = [ '250어7142', '142가7415', '888호8888' ];
 주차 위치 목록: let locationArray = [ 'A1', 'B3', 'C2' ];
-사용자로부터 차량 번호를 입력받아, carArray에서 해당 차량을 찾은 뒤 locationArray에 있는 그 차량의 주차 위치를 찾아 출력하는 프로그램을 작성하시오. 만약 해당하는 차량 번호가 없다면 '차량이 존재하지 않습니다.'를 출력하시오.
+사용자로부터 차량 번호를 입력받아, 
+carArray에서 해당 차량을 찾은 뒤 locationArray에 있는 그 차량의 주차 위치를 찾아 출력하는 프로그램을 작성하시오. 
+만약 해당하는 차량 번호가 없다면 '차량이 존재하지 않습니다.'를 출력하시오.
 */
 
+/*
+let carArray = [ '250어7142', '142가7415', '888호8888' ];
+let locationArray = [ 'A1', 'B3', 'C2' ];
+
+let carNum = prompt("차량 번호를 입력해주세요.")
+
+let carIndex = carArray.indexOf(carNum);
+
+if ( carIndex >=0 && carIndex <=2 ) {
+    console.log(`주차위치 : ${locationArray[carIndex]}`);
+} else {
+    console.log( "차량이 존재하지 않습니다." );
+}
+*/
 
 /* ==============================
 문제 10: 수강 신청 목록에서 과목 제외하기
 현재 수강 신청한 과목 목록이 배열로 주어져 있습니다.
 let courseList = ['수학', '영어', '과학', '국어'];
-사용자로부터 제외하고 싶은 과목명을 입력받아, courseList에 해당 과목이 존재하면 목록에서 삭제한 뒤 변경된 배열 전체를 출력하세요. 만약 존재하지 않는 과목이면 '해당 과목은 신청 목록에 없습니다.'를 출력하는 프로그램을 작성하시오.
+사용자로부터 제외하고 싶은 과목명을 입력받아, 
+courseList에 해당 과목이 존재하면 목록에서 삭제한 뒤 변경된 배열 전체를 출력하세요. 
+만약 존재하지 않는 과목이면 '해당 과목은 신청 목록에 없습니다.'를 출력하는 프로그램을 작성하시오.
 */
+/*
+let courseList = ['수학', '영어', '과학', '국어'];
+
+let subject = prompt("제외하고 싶은 과목명을 입력해주세요.");
+
+let courseIndex = courseList.indexOf(subject);
+
+if (courseIndex >= 0 && courseIndex <= 3) {
+    courseList.splice(courseIndex,1);
+    console.log(courseList);
+} else {
+    console.log("해당 과목은 신청 목록에 없습니다.");
+}
+    */
