32751a84ee4af9f7b7f434fc17fbf60bde903e6b
OngTK <dhdxorud@gmail.com>
2025-06-26 14:58:02 +0900

    250626_1457

--- 02_Daily/03_JS/day22_20250626/board/js/update.js
+++ 02_Daily/03_JS/day22_20250626/board/js/update.js
@@ -30,39 +30,28 @@ function boardUpdate() {
     const url = new URLSearchParams(location.search);
     const selectNo = url.get('no');
 
-    // 게시물의 수정된 제목, 내용, 비밀번호를 가져옴
-    const titleInput = document.querySelector('#titleInput');
-    const contentInput = document.querySelector('#contentInput');
-    const pwdInput = document.querySelector('#pwdInput');
-
-    const title = titleInput.value;
-    const content = contentInput.value;
-    const pwd = pwdInput.value;
-
-    //수정된 게시물 객체 생성
-    const obj = { no: selectNo , title, content, pwd }
-
     //localStorage에서 boardList를 가져옴
-    let boardList = localStorage.getItem("boardList")
+    let boardList = localStorage.getItem("boardList");
     if (boardList == null) {
         boardList = [];
     } else {
         boardList = JSON.parse(boardList);
-    }
+    };
 
     // 게시물의 no와 수정 객체의 no가 같을 경우,
     // 해당 객체를 replace
-    for(let i = 0 ; i < boardList.length ; i++){
-        const board = boardList[i]
-        if(board.no == obj.no){
-            boardList[i] = obj
-        }
-    }
+    for (let i = 0; i < boardList.length; i++) {
+        const board = boardList[i];
+        if (board.no == selectNo) {
+            // 게시물의 수정된 제목, 내용, 비밀번호를 가져옴
+            board.title = document.querySelector('#titleInput').value;
+            board.content = document.querySelector('#contentInput').value;
+            board.pwd = document.querySelector('#pwdInput').value;
 
-    //localstorage에 저장
-    localStorage.setItem("boardList", JSON.stringify(boardList))
-
-    alert('[성공] 게시물 수정 완료')
-    
-    location.href = 'list.html'
-}
+            //localstorage에 저장
+            localStorage.setItem("boardList", JSON.stringify(boardList))
+            alert('[성공] 게시물 수정 완료')
+        };
+    };
+    location.href = `view.html?no=${selectNo}`
+};
